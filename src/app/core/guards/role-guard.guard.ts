24714dbebfd4f3e105abff97ef9cfd1f40b62c35
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2022-08-29 21:52:16 +0100

    toast issue

--- src/app/core/guards/role-guard.guard.ts
+++ src/app/core/guards/role-guard.guard.ts
@@ -15,9 +15,9 @@ export class RoleGuard implements CanActivate {
   canActivate(route: ActivatedRouteSnapshot): boolean {
     const user: any = localStorage.getItem("current-user");
     if (!this.authS.userAuthenticated() || user?.userType === "admin") {
-      this.toastrService.info("You need admin priviledges to view this page", '', {
-        timeOut: 3000,
-      });
+      // this.toastrService.info("You need admin priviledges to view this page", '', {
+      //   timeOut: 3000,
+      // });
       console.log("You need admin priviledges to view this page");
       this.router.navigate(["pages/login"]);
       return false;
