09e2ad6a901496806b52ca6d6b5651ddc99740b1
Sir Abubakar <60545690+Abubakar-Abdulwahab@users.noreply.github.com>
2023-06-12 13:46:16 +0100

    updates referral table

--- src/app/admin-dashboard/components/users/list-user/list-user.component.ts
+++ src/app/admin-dashboard/components/users/list-user/list-user.component.ts
@@ -1,16 +1,16 @@
-import { AuthService } from './../../../../core/service/auth/auth.service';
-import { Component, OnInit } from '@angular/core';
-import * as e from 'express';
-import { ToastrService } from 'ngx-toastr';
+import { AuthService } from "./../../../../core/service/auth/auth.service";
+import { Component, OnInit } from "@angular/core";
+import * as e from "express";
+import { ToastrService } from "ngx-toastr";
 // import { userListDB } from 'src/app/shared/tables/list-users';
 
 @Component({
-  selector: 'app-list-user',
-  templateUrl: './list-user.component.html',
-  styleUrls: ['./list-user.component.scss']
+  selector: "app-list-user",
+  templateUrl: "./list-user.component.html",
+  styleUrls: ["./list-user.component.scss"],
 })
 export class ListUserComponent implements OnInit {
-  public user_list = []
+  public user_list = [];
   referrals: any;
 
   constructor(private authS: AuthService, private toastS: ToastrService) {
@@ -21,37 +21,37 @@ export class ListUserComponent implements OnInit {
     actions: {
       edit: false,
       add: false,
-      position: 'right'
-  },
+      position: "right",
+    },
     delete: {
       confirmDelete: true,
 
-      deleteButtonContent: 'Delete data',
-      saveButtonContent: 'save',
-      cancelButtonContent: 'cancel'
+      deleteButtonContent: "Delete data",
+      saveButtonContent: "save",
+      cancelButtonContent: "cancel",
     },
     edit: {
       editButtonContent: `'<i class="fas fa-pencil-alt fa-fw"></i>'`,
       saveButtonContent: '<i class="fas fa-check fa-fw"></i>',
       cancelButtonContent: '<i class="fas fa-times fa-fw"></i>',
-      confirmSave: true
+      confirmSave: true,
     },
     columns: {
       fullName: {
-        title: 'Name',
+        title: "Name",
       },
       email: {
-        title: 'Email'
+        title: "Email",
       },
       phone: {
-        title: 'Phone'
+        title: "Phone",
       },
       userType: {
-        title: 'Role'
+        title: "Role",
       },
-      'id': {
-        title: 'Edit',
-        type: 'html'
+      id: {
+        title: "Edit",
+        type: "html",
       },
     },
   };
@@ -60,15 +60,15 @@ export class ListUserComponent implements OnInit {
       edit: false,
       add: false,
       delete: false,
-      position: 'right'
-  },
-  delete: {
-    confirmDelete: true,
+      position: "right",
+    },
+    delete: {
+      confirmDelete: true,
 
-    deleteButtonContent: 'Delete data',
-    saveButtonContent: 'save',
-    cancelButtonContent: 'cancel'
-  },
+      deleteButtonContent: "Delete data",
+      saveButtonContent: "save",
+      cancelButtonContent: "cancel",
+    },
     // edit: {
     //   editButtonContent: `'<i class="fas fa-pencil-alt fa-fw"></i>'`,
     //   saveButtonContent: '<i class="fas fa-check fa-fw"></i>',
@@ -77,66 +77,58 @@ export class ListUserComponent implements OnInit {
     // },
     columns: {
       referrer: {
-        title: 'Referer name',
+        title: "Referer name",
       },
       referrerToken: {
-        title: 'Referrer token'
+        title: "Referrer token",
       },
       referee: {
-        title: 'Referee name'
+        title: "Referee name",
       },
       createdAt: {
-        title: 'Created at'
+        title: "Created at",
       },
     },
   };
 
   ngOnInit() {
-   
-    this.loadUsers()
+    this.loadUsers();
   }
-  loadUsers(){
-    this.authS.getUsers().subscribe(res =>{
-      this.user_list = res.data.map(e =>{
+  loadUsers() {
+    this.authS.getUsers().subscribe((res) => {
+      this.user_list = res.data.map((e) => {
         return {
           ...e,
           userType: e.userType.toUpperCase(),
           id: `
           <a href=dashboard/main/users/edit-user/${e._id} class="ng2-smart-action ng2-smart-action-edit-edit ng-star-inserted"><a/>
-          `
-        }
-      })
-      console.log(res)
-    })
-    this.authS.getReferrals().subscribe(res =>{
-
-      this.referrals = res.data.map(e =>{
-        return {
-          createdAt: new Date(e.createdAt).toLocaleDateString(),
-          referee: e.referee.fullName,
-          referrer: e.referrer.fullName,
-          referrerToken: e.referrer.referrerToken,
-          // id: `
-          // <a href=dashboard/main/users/edit-user/${e._id} class="ng2-smart-action ng2-smart-action-edit-edit ng-star-inserted"><a/>
-          // `
-        }
-      })
-      console.log(res)
-    })
+          `,
+        };
+      });
+      console.log(res);
+    });
+    this.authS.getReferrals().subscribe((res) => {
+      this.referrals = res.data
+        .filter((e) => e.referee !== null)
+        .map(({ createdAt, referee, referrer }) => ({
+          createdAt: new Date(createdAt)?.toLocaleDateString(),
+          referee: referee?.fullName,
+          referrer: referrer?.fullName,
+          referrerToken: referrer?.referrerToken,
+        }));
+      console.log(res);
+    });
   }
   onDeleteConfirm(event) {
-    if (window.confirm('Are you sure you want to delete?')) {
-      this.authS.deleteUser(event.data._id).subscribe(e =>{
+    if (window.confirm("Are you sure you want to delete?")) {
+      this.authS.deleteUser(event.data._id).subscribe((e) => {
         event.confirm.resolve();
-        this.toastS.success("Successfully deleted user")
-        console.log(e)
-        this.loadUsers()
-
-      })
+        this.toastS.success("Successfully deleted user");
+        console.log(e);
+        this.loadUsers();
+      });
     } else {
       event.confirm.reject();
     }
   }
-
 }
-
